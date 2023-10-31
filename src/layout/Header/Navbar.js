74fc3ac96257e6494b2e1fc80124c090ab7c61c4
kuccii <34752778+kuccii@users.noreply.github.com>
2023-10-31 07:25:35 +0200

    Update Navbar.js

--- src/layout/Header/Navbar.js
+++ src/layout/Header/Navbar.js
@@ -194,10 +194,10 @@ const Navbar = ({ navDark, insurance, classOption }) => {
           </div>
 
           <div className="action-btns text-end me-5 me-lg-0 d-none d-md-block d-lg-block">
-            <Link href="https://qivook.surge.sh/">
+            <Link href="https://qivook.vercel.app/">
               <a className="btn btn-link text-decoration-none me-2">Sign In</a>
             </Link>
-            <Link href="https://qivook.surge.sh/">
+            <Link href="https://qivook.vercel.app/auth/register">
               <a
                 className={
                   insurance ? 'ins-btn ins-primary-btn' : 'btn btn-primary'
