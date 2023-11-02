b9866e525cac754ef9add3f072233602c437c42c
kuccii <34752778+kuccii@users.noreply.github.com>
2023-11-02 11:20:10 +0200

    Update Navbar.js

--- src/layout/Header/Navbar.js
+++ src/layout/Header/Navbar.js
@@ -194,7 +194,7 @@ const Navbar = ({ navDark, insurance, classOption }) => {
           </div>
 
           <div className="action-btns text-end me-5 me-lg-0 d-none d-md-block d-lg-block">
-            <Link href="https://qivook.vercel.app/">
+            <Link href="http://qivook.adadieworld.com">
               <a className="btn btn-link text-decoration-none me-2">Sign In</a>
             </Link>
             <Link href="https://qivook.vercel.app/auth/register">
