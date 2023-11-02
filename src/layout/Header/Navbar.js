f5b78cf787fdadeb12c702b07ef1e4b31990e2ba
kuccii <34752778+kuccii@users.noreply.github.com>
2023-11-02 13:45:16 +0200

    Update Navbar.js

--- src/layout/Header/Navbar.js
+++ src/layout/Header/Navbar.js
@@ -194,7 +194,7 @@ const Navbar = ({ navDark, insurance, classOption }) => {
           </div>
 
           <div className="action-btns text-end me-5 me-lg-0 d-none d-md-block d-lg-block">
-            <Link href="http://qivook.adadieworld.com">
+            <Link href="http://qivook.adadieworld.com/auth">
               <a className="btn btn-link text-decoration-none me-2">Sign In</a>
             </Link>
             <Link href="http://qivook.adadieworld.com/auth/register">
