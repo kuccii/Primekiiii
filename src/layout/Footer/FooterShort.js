dfa0bdc84b7442f85daab686ffbdc2e70e30217a
kuccii <34752778+kuccii@users.noreply.github.com>
2023-11-04 19:14:45 +0200

    Update FooterShort.js

--- src/layout/Footer/FooterShort.js
+++ src/layout/Footer/FooterShort.js
@@ -12,10 +12,8 @@ const FooterShort = () => {
             <div className="col-md-7 col-lg-7">
               <div className="copyright-text">
                 <p className="mb-lg-0 mb-md-0">
-                  &copy; 2022 Quiety Rights Reserved. Designed By{' '}
-                  <Link href="https://themetags.com/">
-                    <a className="text-decoration-none">ThemeTags</a>
-                  </Link>
+                  &copy; 2022 Quiety Rights Reserved. Designed 
+                  
                 </p>
               </div>
             </div>
