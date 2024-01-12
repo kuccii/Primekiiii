12b789ce2b10ab1463d6a5bd378ab854f6ac0855
kuccii <34752778+kuccii@users.noreply.github.com>
2024-01-12 10:33:52 +0300

    Update OffCanvasMenu.js

--- src/layout/Header/OffCanvasMenu.js
+++ src/layout/Header/OffCanvasMenu.js
@@ -48,7 +48,7 @@ const OffCanvasMenu = () => {
           </div>
         </li> */}
          <li data-bs-dismiss="offcanvas" aria-label="Close">
-          <Link href="/index">
+          <Link href="https://www.qivook.com/">
             <a className="nav-link">Home</a>
           </Link>
         </li>
