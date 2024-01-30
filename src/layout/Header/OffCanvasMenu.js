ec49289a73b7e50f167cbeba966c68d1609941a9
kuccii <34752778+kuccii@users.noreply.github.com>
2024-01-30 16:17:10 +0200

    Update OffCanvasMenu.js

--- src/layout/Header/OffCanvasMenu.js
+++ src/layout/Header/OffCanvasMenu.js
@@ -47,7 +47,7 @@ const OffCanvasMenu = () => {
             </div>
           </div>
         </li> */}
-         <li data-bs-dismiss="offcanvas" aria-label="Close">
+        <li data-bs-dismiss="offcanvas" aria-label="Close">
           <Link href="/">
             <a className="nav-link">Home</a>
           </Link>
@@ -62,7 +62,6 @@ const OffCanvasMenu = () => {
             <a className="nav-link">Services</a>
           </Link>
         </li>
-
         <li data-bs-dismiss="offcanvas" aria-label="Close">
           <Link href="/pricing">
             <a className="nav-link">Pricing</a>
@@ -72,6 +71,7 @@ const OffCanvasMenu = () => {
           <Link href="#">
             <a className="nav-link">Company</a>
           </Link>
+        </li>
          <li className="nav-item dropdown">
           <a
             className="nav-link"
@@ -134,7 +134,7 @@ const OffCanvasMenu = () => {
             </a>
           </Link>
         </span>
-        <span data-bs-dismiss="offcanvas" aria-label="Close">
+       <span data-bs-dismiss="offcanvas" aria-label="Close">
           <Link href="/request-demo">
             <a className="btn btn-primary">Get Started</a>
           </Link>
